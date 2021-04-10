1d0abb917f91889a19736c8c9d4d4ef27310f081
Divanshu Agarwal <divanshuagarwal2001@gmail.com>
2021-04-11 00:45:19 +0530

    added profile page

--- src/routes/Navigator.js
+++ src/routes/Navigator.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Landing from "../pages/landing/Landing";
-import Home from "../pages/home/Home";
 import Stream from "../pages/stream/stream";
+import Home from "../pages/home/Home";
+import Profile from "../pages/profile/profile";
 import { SocketContextProvider } from "../context/SocketContext";
 const Navigator = () => {
   return (
@@ -11,6 +12,7 @@ const Navigator = () => {
         <Switch>
           <Route path="/" exact component={() => <Landing />} />{" "}
           <Route path="/home" exact component={() => <Home />} />{" "}
+          <Route path="/profile/:id" exact component={() => <Profile />} />{" "}
           <Route
             path="/stream"
             exact
