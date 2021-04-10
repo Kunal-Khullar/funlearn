710bec5e51f012da6ae86db629d7654dbe03f400
Divanshu Agarwal <divanshuagarwal2001@gmail.com>
2021-04-10 22:13:53 +0530

    register and landing fix

--- src/pages/landing/Landing.js
+++ src/pages/landing/Landing.js
@@ -1,20 +1,44 @@
 import React, { useState } from "react";
 import Mynav from "../../components/navbar/Mynav";
 import "./landing.css";
-import { Row, Col, Button, Modal } from "react-bootstrap";
-import Footer from '../../components/footer/Footer'
-import Register from '../../components/register/Register'
+import Register from "../../components/register/Register";
 import gifImage from "./learning.gif";
+import Footer from "../../components/footer/Footer";
+// import Login from '../../components/login/Login'
+import { fireapp, auth, db } from "../../firebase";
+import { Row, Col, Button, Modal } from "react-bootstrap";
+import AOS from "aos";
+import "aos/dist/aos.css";
+AOS.init();
 const Landing = () => {
+  const [lgShow, setLgShow] = useState(false);
   return (
-    
     <div>
-  <Mynav/>
-  
+      <Modal
+        size="lg"
+        show={lgShow}
+        dialogClassName="modal-40w"
+        onHide={() => setLgShow(false)}
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-modal-sizes-title-lg">
+            Create an account
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Register />
+        </Modal.Body>
+      </Modal>
+      <Mynav />
       <div
         className="top_container"
         id="top"
-       
+        data-aos="flip-down"
+        data-aos-delay="50"
+        data-aos-duration="1000"
+        data-aos-easing="ease-in-out-cubic"
       >
         <Row>
           <Col>
@@ -27,12 +51,12 @@ const Landing = () => {
               industry ever since the 1500s, when an unknown printer took a
               galley of type and scrambled it to make a type specimen book.
             </p>
-            <Button variant="outline-light">
+            <Button onClick={() => setLgShow(true)} variant="outline-light">
               Register
             </Button>
           </Col>
           <Col>
-          <img src={gifImage} alt=""></img>
+            <img src={gifImage} alt=""></img>
           </Col>
         </Row>
       </div>
@@ -41,7 +65,10 @@ const Landing = () => {
           <Col>
             <div
               className="services"
-              
+              data-aos="fade-right"
+              data-aos-delay="50"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out-cubic"
             >
               <div className="imgcontainer1">
                 <img src="https://img.icons8.com/dusk/64/000000/e-learning.png" />
@@ -58,7 +85,10 @@ const Landing = () => {
           <Col>
             <div
               className="services"
-             
+              data-aos="fade-down"
+              data-aos-delay="50"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out-cubic"
             >
               <div className="imgcontainer2">
                 <img src="https://img.icons8.com/plasticine/100/000000/teacher.png" />
@@ -77,7 +107,10 @@ const Landing = () => {
           <Col>
             <div
               className="services"
-          
+              data-aos="fade-left"
+              data-aos-delay="50"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out-cubic"
             >
               <div className="imgcontainer3">
                 <img src="https://img.icons8.com/flat-round/64/000000/three-stars.png" />
@@ -94,8 +127,90 @@ const Landing = () => {
           </Col>
         </Row>
       </div>
-     <Footer></Footer>
-     
+      <div className="bot_container" id="bot">
+        <div className="steps">
+          <Row>
+            <Col
+              data-aos="zoom-out-right"
+              data-aos-delay="50"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out-cubic"
+            >
+              <h1>Ask your Doubts</h1>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged.
+              </p>
+            </Col>
+            <Col>
+              <img
+                id="img1"
+                src="https://image.freepik.com/free-vector/online-courses-concept_23-2148533386.jpg"
+                alt=""
+              ></img>
+            </Col>
+          </Row>
+        </div>
+        <div className="steps">
+          <Row>
+            <Col>
+              <img
+                id="img2"
+                src="https://image.freepik.com/free-vector/online-learning-concept-illustration_114360-4755.jpg"
+                alt=""
+              ></img>
+            </Col>
+            <Col
+              data-aos="zoom-out-left"
+              data-aos-delay="50"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out-cubic"
+            >
+              <h1>Resolve others' issues.</h1>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged.
+              </p>
+            </Col>
+          </Row>
+        </div>
+        <div className="steps">
+          <Row>
+            <Col
+              data-aos="zoom-out-right"
+              data-aos-delay="50"
+              data-aos-duration="1000"
+              data-aos-easing="ease-in-out-cubic"
+            >
+              <h1>Get feedback</h1>
+              <p>
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. Lorem Ipsum has been the industry's standard dummy
+                text ever since the 1500s, when an unknown printer took a galley
+                of type and scrambled it to make a type specimen book. It has
+                survived not only five centuries, but also the leap into
+                electronic typesetting, remaining essentially unchanged.
+              </p>
+            </Col>
+            <Col>
+              <img
+                id="img3"
+                src="https://image.freepik.com/free-vector/online-survey-analysis-electronic-data-collection-digital-research-tool-computerized-study-analyst-considering-feedback-results-analyzing-info_335657-2385.jpg"
+                alt=""
+              ></img>
+            </Col>
+          </Row>
+        </div>
+        <Footer />
+      </div>
     </div>
   );
 };
